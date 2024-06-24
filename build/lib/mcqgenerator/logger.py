b2922d3d27eae022248c2a118c6f5e78cdff9e78
raghava <raghava000@gmail.com>
2024-06-24 21:36:33 +0530

    files updated

--- build/lib/mcqgenerator/logger.py
+++ build/lib/mcqgenerator/logger.py
@@ -4,10 +4,10 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%M_%S')}.log"
 
-log_path = os.path.join(os.getcwd(),"logs")
+log_path = os.join(os.getcwd(),"logs")
 
 os.makedirs(log_path,exist_ok=True)
 
 LOG_FILEPATH = os.path.join(log_path,LOG_FILE)
 
-logging.basicConfig(level = logging.INFO, filename = LOG_FILEPATH, format= "[%(asctime)s] %(lineno)s - %(levelname)s - %(message)s")
+logging(level = logging.INFO, filename = LOG_FILEPATH, format= "[%(ascime)s] %(lineno)s - %(levelname)s - %(message)s")
--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version = '0.0.1',
     author = 'raghava',
     author_email = 'raghava000@gmail.com',
-    install_requires = ["openai","langchain", "streamlit","pythion-dotenv", "PyPDF2"],
-    packages=find_packages()
+    install_requires = ["openai","langchain", "streamlit","python-dotenv", "PyPDF2"],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"}
 )
+
